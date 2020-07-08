bfda4ca9c528c93f9b9782c59d94a4b20babaea9
xzdd@xzdds-MBP <56295612@qq.com>
2020-07-09 01:25:51 +0800

    Is there a version difference?

--- PalindromeSmall.sh
+++ PalindromeSmall.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-read word
-word="$(echo $word | tr -d -c a-zA-Z)"
+# PalindromeSmall
+# Author: Xu Zhen
+# 在本实现中，我们直接调用了tr命令来剔除不合要求的字符串
+
+# 本语句读入一行用户输入内容并删除非字母内容，复制给word变量
+word="$(read word; echo $word | tr -d -c a-zA-Z)"
+# 本语句反转word变量，赋值给reve变量
 reve="$(echo $word | rev)"
+
+# DEBUG INFO
 # echo $word
 # echo $reve
+
+# 本语句进行回文测试并打印测试结果
 [ $word = $reve ] && echo "True" || echo "False"
--- WordCount.sh
+++ WordCount.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # WordCount
-echo "My name is: $0"
-echo "First argument is: $1"
-# echo "Second argument is: $2"
+
+# 统计目录文件个数
 dir_num="$(ls -l $1|grep ^d|wc -l|xargs)"
 regular_num="$(ls -l $1|grep ^-|wc -l|xargs)"
-exec_num="$(find $1 -type f -perm +111 -not \( -path "*/\.*" -o -path "\." \) |wc -l|xargs)"
-regular_sum="$(find $1 -type f -exec du -c {} + | grep total | tr -d -c 0-9)"
+exec_num="$(find $1 -type f -perm /111 -not \( -path "*/\.*" -o -path "\." \) | wc -l | xargs)"
+regular_sum="$(find $1 -type f -exec du -cb {} + -not \( -path "*/\.*" -o -path "\." \) | grep total | tr -d -c 0-9)"
 echo "Number of directories: $dir_num"
 echo "Number of regular files: $regular_num"
 echo "Number of executable files: $exec_num"
-echo "Total size of regular files: ${regular_sum} B"
+echo "Total size of regular files: ${regular_sum} Byte(s)"
