7b910ca8dbd3e1f63ef8366eb70c1717f329e4a5
xzdd@xzdds-MBP <56295612@qq.com>
2020-07-23 11:18:25 +0800

    妈的，成了！

--- MyShell/dummy.py
+++ MyShell/dummy.py
@@ -1,39 +1,63 @@
-import multiprocessing
-import readline
-class MyFancyClass(object):
-    
-    def __init__(self, name):
-        self.name = name
-    
-    def do_something(self):
-        proc_name = multiprocessing.current_process().name
-        print('Doing something fancy in %s for %s!' % (proc_name, self.name))
-
-
-def worker(q):
-    obj = q.get()
-    obj.do_something()
-
-
-if __name__ == '__main__':
-    queue = multiprocessing.Queue()
-
-    p = multiprocessing.Process(target=worker, args=(queue,))
-    p.start()
-    
-    queue.put(MyFancyClass(input("myshell> ")))
-    
-    # Wait for the worker to finish
-    queue.close()
-    queue.join_thread()
-    p.join()
-    
-# import sys
+# import readline
 # import multiprocessing
-# def func():
-#     sys.stdin=open(0)
-#     print(sys.stdin)
-#     c = sys.stdin.read(1)
-#     print("Got", c)
+# class MyFancyClass(object):
+    
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def do_something(self):
+#         proc_name = multiprocessing.current_process().name
+#         print('Doing something fancy in %s for %s!' % (proc_name, self.name))
 
-# multiprocessing.Process(target=func).start()
+
+# def worker(q):
+#     obj = q.get()
+#     obj.do_something()
+
+
+# if __name__ == '__main__':
+#     queue = multiprocessing.Queue()
+
+#     p = multiprocessing.Process(target=worker, args=(queue,))
+#     p.start()
+    
+#     queue.put(MyFancyClass(input("myshell> ")))
+    
+#     # Wait for the worker to finish
+#     queue.close()
+#     queue.join_thread()
+#     p.join()
+
+import io
+import sys
+from multiprocessing import Process, Queue, Manager, Pool
+
+class Wrapper(io.TextIOWrapper):
+    def __init__(self, queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queue = queue
+    def read(self, size):
+        self.queue.get()
+        print("HAHA! GOTHA!")
+        return super().read(size)
+    
+def func(queue):
+    if sys.stdin is not None:
+        sys.stdin.close()
+    stdin=open(0)
+    buffer = stdin.detach()
+    wrapper = Wrapper(queue, buffer)
+    sys.stdin = wrapper
+    print(sys.stdin)
+    c = sys.stdin.read(1)
+    print("Got", c)
+    q.put("dummy")
+
+q = Queue()
+Process(target=func, args=(q,)).start()
+print("Before putting stuff in the queue, the stdin should be responding to main process")
+print(input("myshell> "))
+print("Should wait for wrapper to finish")
+q.put("dummy")
+q.get()
+print(input("myshell> "))
